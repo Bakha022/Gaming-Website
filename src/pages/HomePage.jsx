7bb22461df5244c4171e72c3e587d1dc134d0af2
Asadullohbek05 <ayubovasadbek33@gmail.com>
2024-06-29 23:30:53 +0500

    Navbar Desktop

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,10 +4,8 @@ import { Container } from 'react-bootstrap'
 export default class HomePage extends Component {
 	render() {
 		return (
-			// <Fragment></Fragment>
 			<Container>
-				<h1 className='text-center'>HomePage</h1>
-				<p className='text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus aliquam temporibus quia? Dolore quidem explicabo et, cumque nihil tenetur officiis. Aliquam, pariatur numquam? Dicta quo voluptates dolorum explicabo maiores voluptatum.</p>
+				<h1 className='mt-5'>HomePage</h1>
 			</Container>
 		)
 	}
