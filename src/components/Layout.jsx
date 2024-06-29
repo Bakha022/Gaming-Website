74022ca97d6e9698ccf581259f8a0dcd4ca053d7
Asadullohbek05 <ayubovasadbek33@gmail.com>
2024-06-30 00:24:41 +0500

    footer finished

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { Component, Fragment } from 'react'
 import { Outlet } from 'react-router-dom'
-import FooterComponent from './FooterComponent'
 import NavbarComponent from './navbar/NavbarComponent';
+import FooterComponent from './footer/FooterComponent';
 
 export default class Layout extends Component {
 	render() {
