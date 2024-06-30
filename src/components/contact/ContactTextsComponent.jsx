c95e15d95de52e56d1894e378f485691ccd0b9d5
Asadullohbek05 <ayubovasadbek33@gmail.com>
2024-06-30 16:07:38 +0500

    Hero , Tranding Games and Banner Sections Added

--- src/components/contact/ContactTextsComponent.jsx
+++ src/components/contact/ContactTextsComponent.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 
-import { FaChevronRight } from "react-icons/fa";
+
+import { FaChevronRight } from 'react-icons/fa';
 
 
 import './ContactTextsComponent.module.css'
@@ -10,15 +11,15 @@ export default class ContactTextsComponent extends Component {
 		return (
 			<div className="container text-white">
 				<div className='d-flex  justify-content-center'>
-				<p className='first-text  d-flex  align-items-center gap-1 aligin-center ustify-content-center'>	
+					<p className='first-text  d-flex  align-items-center gap-1 aligin-center ustify-content-center'>
 						<span className='location-head'>Home</span>
 						<FaChevronRight size={16} />
 						<span className='location-main'>Contact us</span>
-				</p>
+					</p>
 				</div>
 				<h2>Lorem Ipsum is simply dummy text of the printing and.</h2>
 				<p className='second-text'>
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
 				</p>
 			</div>
 		)
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,20 @@
-import { Component, } from 'react'
+import { Component, Fragment, } from 'react'
+
+import HomeHero from '../components/HomeHero/HomeHero'
+import TrandingGames from '../components/TrandingGames/TrandingGames'
+import CallOfDuty from '../components/CallOfDuty/CallOfDuty'
+import HomeBanner from '../components/HomeBanner/HomeBanner'
+
 
 export default class HomePage extends Component {
 	render() {
 		return (
-			<div className='container'>
-				<h1 className='my-5 text-white text-center'>HomePage</h1>
-				<p className='text-white text-center mb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, possimus labore. Vero ea dolore est fugit? Ipsum quos non, vitae esse aspernatur assumenda laudantium vel quod. Minima velit quo officiis mollitia architecto, corrupti eaque consequuntur magnam quam, quae facere ea quod quas dolor illum doloribus consequatur reiciendis neque. Culpa placeat accusantium porro id magnam quam.</p>
-			</div>
+			<Fragment>
+				<HomeHero />
+				<TrandingGames />
+				<CallOfDuty />
+				<HomeBanner />
+			</Fragment>
 		)
 	}
 }
