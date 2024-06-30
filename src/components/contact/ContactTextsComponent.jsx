c28c148f91d3b4e166872e1860b24006f0e10ce5
Bakha022 <abduqahhorovbahodir536@gmail.com>
2024-06-30 21:29:54 +0500

    asadbekdan pull qildim

--- src/components/contact/ContactTextsComponent.jsx
+++ src/components/contact/ContactTextsComponent.jsx
@@ -1,7 +1,8 @@
 import { Component } from 'react'
 
+import { FaChevronRight } from "react-icons/fa"
 
-import { FaChevronRight } from "react-icons/fa";
+import { FaChevronRight } from 'react-icons/fa';
 
 
 import style from './ContactTextsComponent.module.css'
@@ -11,15 +12,22 @@ export default class ContactTextsComponent extends Component {
 		return (
 			<div className="container">
 				<div className='d-flex  justify-content-center'>
-				<p className='first-text  d-flex  align-items-center gap-1 aligin-center ustify-content-center'>	
+					<p className='first-text  d-flex  align-items-center gap-1 aligin-center ustify-content-center'>
+						<span className={style['location-head']}>Home</span>
+						<FaChevronRight className='text-white' size={14} />
+						<span className={style['location-main']}>Contact us</span>
+					</p>
+				</div>
+				<h2 className={style['contact-title']}>Lorem Ipsum is simply dummy text of the printing and.</h2>
+				<p className={style['second-text']}>
 						<span className='location-head'>Home</span>
 						<FaChevronRight size={16} />
 						<span className='location-main'>Contact us</span>
-				</p>
+					</p>
 				</div>
 				<h2>Lorem Ipsum is simply dummy text of the printing and.</h2>
 				<p className='second-text'>
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
 				</p>
 			</div>
 		)
