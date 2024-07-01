8c04b09fa4e487de1f9535ffcf8531602a534218
Arslon004 <arslonbozorov2109@gmail.com>
2024-07-01 11:54:05 +0300

    change responsive

--- src/components/ServicesFooter/ServicesFooter.jsx
+++ src/components/ServicesFooter/ServicesFooter.jsx
@@ -5,8 +5,10 @@ import styles from "./ServicesFooter.module.css";
 export class ServicesFooter extends Component {
   render() {
     return (
-      <div className={`container ${styles.services__footer} `}>
+      <div className={`container `}>
+        <div className={styles.services__footer}>
           <Subscribe formColor={'#ffffff'} textColors={'#000000'} inputColor={'#000000'} buttonColor={'#D80027'}/>
+        </div>
       </div >
     )
   }
