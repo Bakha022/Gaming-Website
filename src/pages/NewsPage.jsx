e36e2bd646de03bc615f393dbb8c29dab84514df
Muhammaddiyor007 <odiljonovmuhammaddiyor63@gmail.com>
2024-07-02 15:42:34 +0500

    News

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,9 +1,22 @@
 import { Component, Fragment } from 'react'
+import Hero from '../components/heroNews/Hero'
+import Card from '../components/cardNews/NewsCard'
+import Subscribe from '../components/subscribe/Subscribe'
 
 export default class NewsPage extends Component {
 	render() {
 		return (
-			<Fragment>NewsPage</Fragment>
+			<Fragment>
+
+				<Hero />
+				<section>
+					<Card />
+				</section>
+				<div className='container'>
+					<Subscribe />
+
+				</div>
+			</Fragment>
 		)
 	}
 }
