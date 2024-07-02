1795de5ca5b9ced15a1f4374ba13f8e4d38c5b2e
Bakha022 <abduqahhorovbahodir536@gmail.com>
2024-07-03 01:02:50 +0500

    contact end

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react'
+import FormComponent from '../components/Form/FormComponent'
 import WorldMapComponent from '../components/WorldMap/WorldMapComponent'
 import ContactTextsComponent from '../components/contact/ContactTextsComponent'
+import Subscribe from '../components/subscribe/Subscribe'
 
 export default class ContactPage extends Component {
 	render() {
@@ -10,6 +12,17 @@ export default class ContactPage extends Component {
 					<div className='contact-top'>
 						<ContactTextsComponent />
 						<WorldMapComponent />
+					</div>
+					<FormComponent />
+					<div className='contact-lorem'>
+						<div className='container'>
+							<Subscribe
+								formColor={'#D6C5BB'}
+								textColors={'#000000'}
+								inputColor={'#000000'}
+								buttonColor={'#D80027'}
+							/>
+						</div>
 					</div>
 				</div>
 			</Fragment>
